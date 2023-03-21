c45ba6b1471743fb7d40b5dcb7f42c0cbae22400
LaurenMcQuillan <115508658+LaurenMcQuillan@users.noreply.github.com>
2023-03-21 10:13:59 -0400

    Update ERD

--- ERD/Table_Creation.sql
+++ ERD/Table_Creation.sql
@@ -1,26 +1,22 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "NFL" (
-    "Year" INT   NOT NULL,
-    "Player" VARCHAR   NOT NULL,
-    "Age" INT   NOT NULL,
-    "School" VARCHAR   NOT NULL,
-    "Height" FLOAT   NOT NULL,
-    "Weight" FLOAT   NOT NULL,
-    "Sprint_40yd" FLAOT   NOT NULL,
-    "Vertical_Jump" FLOAT   NOT NULL,
-    "Bench_Press_Reps" INT   NOT NULL,
-    "Broad_Jump" FLOAT   NOT NULL,
-    "Agility_3cone" FLOAT   NOT NULL,
-    "Shuttle" FLOAT   NOT NULL,
-    "Drafted..tm.rnd.yr." VARCHAR   NOT NULL,
-    "BMI" FLOAT   NOT NULL,
-    "Player_Type" VARCHAR   NOT NULL,
-    "Position_Type" VARCHAR   NOT NULL,
-    "Position" VARCHAR   NOT NULL,
-    "Drafted" VARCHAR   NOT NULL
+    "Year" INT,
+    "Player" VARCHAR,
+    "Age" INT,
+    "School" VARCHAR,
+    "Height" NUMERIC,
+    "Weight" NUMERIC,
+    "Sprint_40yd" NUMERIC,
+    "Vertical_Jump" NUMERIC,
+    "Bench_Press_Reps" INT,
+    "Broad_Jump" NUMERIC,
+    "Agility_3cone" NUMERIC,
+    "Shuttle" NUMERIC,
+    "Drafted..tm.rnd.yr." VARCHAR,
+    "BMI" NUMERIC,
+    "Player_Type" VARCHAR,
+    "Position_Type" VARCHAR,
+    "Position" VARCHAR,
+    "Drafted" VARCHAR
 );
 
 CREATE TABLE "Teams_by_Conference" (
